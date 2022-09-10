91ecf4ae7d4a0a3999b1905d9263bda231b73780
Leidy Castaño <leidy.castanoc@udea.edu.co>
2022-09-09 20:35:53 -0500

    paquetes nuevos

--- src/app/service/producto.service.ts
+++ src/app/service/producto.service.ts
@@ -10,7 +10,7 @@ import { Producto } from '../models/producto';
 
 export class ProductoService {
 
-  productoURL = 'http://localhost:8080/producto/';
+  productoURL = 'https://ventas-l.herokuapp.com/producto';
 
   constructor(private httpClient: HttpClient) { }
 
